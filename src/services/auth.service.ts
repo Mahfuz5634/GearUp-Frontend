55f63000297a8bdcea9afd41cff301fb8b70e047
Mahfuz <mahfuzru5634@gmail.com>
2026-08-14 23:18:11 +0600

    Add authentication service with user registration, login, logout, and token management

---
+++ src/services/auth.service.ts
@@ -0,0 +1,37 @@
+import axiosInstance from "@/lib/axiosInstance";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/router";
+
+
+
+export const registerUser = async(userData:any)=>{
+    const {data} = await axiosInstance.post('auth/register',userData);
+    return data;
+}
+
+export const loginUser = async (credentials: any) => {
+  const { data } = await axiosInstance.post('/auth/login', credentials);
+  if (data?.success) {
+    Cookies.set('accessToken', data.data.accessToken, { expires: 30 });
+  }
+  return data;
+};
+
+export const logoutUser =()=>{
+  Cookies.remove('accessToken');
+}
+
+export const getCurrentUser = () => {
+    const token = Cookies.get('accessToken');
+    if (token) {
+      try {
+        const decodedToken: any = jwtDecode(token);
+        return decodedToken;
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        return null;
+      }
+    }
+    return null;
+}
