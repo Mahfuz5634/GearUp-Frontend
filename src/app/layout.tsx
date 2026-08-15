49b4e8d900479cce686aa86110e75d72411abcb4
Mahfuz <mahfuzru5634@gmail.com>
2026-08-15 23:12:28 +0600

    Add Navbar component for navigation and user authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import TanstackProvider from '@/providers/TanstackProvider';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <TanstackProvider>
           {/* navbar */}
+          <Navbar></Navbar>
           <main className="min-h-screen">
             {children}
           </main>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { getCurrentUser, logoutUser } from '@/services/auth.service';
+
+
+export default function Navbar() {
+  const router = useRouter();
+  const [user, setUser] = useState<any>(null);
+
+
+  useEffect(() => {
+    const currentUser = getCurrentUser();
+    setUser(currentUser);
+  }, []);
+
+  const handleLogout = () => {
+    logoutUser();
+    setUser(null);
+    router.push('/auth/login');
+  };
+
+  return (
+    <nav className="bg-slate-900 text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold text-orange-500">
+          GearUp 🏋️
+        </Link>
+
+        <div className="flex gap-6 items-center">
+          <Link href="/gear" className="hover:text-orange-400 transition">Browse Gear</Link>
+          
+          {user ? (
+            <>
+            
+              <Link 
+                href={`/dashboard/${user.role.toLowerCase()}`} 
+                className="hover:text-orange-400 transition"
+              >
+                Dashboard
+              </Link>
+              <button 
+                onClick={handleLogout} 
+                className="bg-red-600 px-4 py-2 rounded-md hover:bg-red-700 transition"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/auth/login" className="hover:text-orange-400 transition">Login</Link>
+              <Link 
+                href="/auth/register" 
+                className="bg-orange-600 px-4 py-2 rounded-md hover:bg-orange-700 transition"
+              >
+                Register
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
