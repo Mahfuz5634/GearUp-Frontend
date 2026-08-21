68abbdbc7544b03911494057741389096934843a
Mahfuz <mahfuzru5634@gmail.com>
2026-08-21 11:47:59 +0600

    feat: remove default Content-Type header from axios instance

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -3,9 +3,6 @@ import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
     baseURL:process.env.NEXT_PUBLIC_API_URL,
-    headers:{
-        'Content-Type':'application/json',
-    },
 });
 
 axiosInstance.interceptors.request.use(
